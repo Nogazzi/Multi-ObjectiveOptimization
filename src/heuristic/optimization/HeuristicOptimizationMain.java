14bed8873f0cdcade935454674ba984de905e64a
Nogazzi <p.noganski@gmail.com>
2017-05-22 21:46:00 +0200

    commit

--- src/heuristic/optimization/HeuristicOptimizationMain.java
+++ src/heuristic/optimization/HeuristicOptimizationMain.java
@@ -1,10 +1,11 @@
 package heuristic.optimization;
 
+import heuristic.optimization.comparators.DominanceCountComparator;
+import heuristic.optimization.comparators.DominanceDepthComparator;
+import heuristic.optimization.comparators.DominanceRankComparator;
+
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.jar.Pack200;
 import java.util.stream.Collectors;
 
@@ -18,8 +19,8 @@ public class HeuristicOptimizationMain {
         simulate(readInd);
         writeToFile(readInd, "base.txt");*/
         List<Individual> initialIndividuals = new ArrayList<>();
-        List<List<Individual>> dominanceRankNaive = new ArrayList<>();
-        List<List<Individual>> dominanceRankKung = new ArrayList<>();
+        //List<List<Individual>> dominanceRankNaive = new ArrayList<>();
+        //List<List<Individual>> dominanceRankKung = new ArrayList<>();
         int individualsAmount = 10;
         for(int i = 0 ; i < individualsAmount ; ++i ){
             initialIndividuals.add(new Individual(2));
@@ -27,7 +28,7 @@ public class HeuristicOptimizationMain {
         List<Individual> initialIndividualsNaive = new ArrayList<>(initialIndividuals);
         List<Individual> initialIndividualsKung = new ArrayList<>(initialIndividuals);
 
-        int counter = 0;
+        /*int counter = 0;
         while( initialIndividualsKung.size() > 0 ) {
             dominanceRankKung.add(new ArrayList<Individual>(simulateByKungAlgorithm(initialIndividualsKung)));
             for( int i = 0 ; i < dominanceRankKung.get(counter).size() ; ++i ){
@@ -37,9 +38,9 @@ public class HeuristicOptimizationMain {
                 }
             }
             counter++;
-        }
+        }*/
 
-        counter = 0;
+        /*counter = 0;
         while( initialIndividualsNaive.size() > 0 ) {
             dominanceRankNaive.add(new ArrayList<Individual>(simulateByNaiveAlgorithm(initialIndividualsNaive)));
             for( int i = 0 ; i < dominanceRankNaive.get(counter).size() ; ++i ){
@@ -49,20 +50,40 @@ public class HeuristicOptimizationMain {
                 }
             }
             counter++;
-        }
+        }*/
+        List<Individual> dominanceRankKung = generateDominanceDepthLayersByKung(initialIndividualsKung);
+        List<Individual> dominanceRankNaive = generateDominanceDepthLayersByNaive(initialIndividualsNaive);
 
-        System.out.println("Kung:");
-        for( int i = 0 ; i < dominanceRankKung.size() ; ++i ){
-            System.out.println((i+1) + ".) " + dominanceRankKung.get(i).size() + " elementów");
-            System.out.println("Dominance depth: " + dominanceRankKung.get(i).get(0).getDominanceDepth());
-        }
+        printSortedByDominanceCount(dominanceRankKung);
+        printSortedByDominanceDepth(dominanceRankKung);
+        printSortedByDominanceRank(dominanceRankKung);
+    }
 
-        System.out.println("Naive:");
-        for( int i = 0 ; i < dominanceRankNaive.size() ; ++i ){
-            System.out.println((i+1) + ".) " + dominanceRankNaive.get(i).size() + " elementów");
-            System.out.println("Dominance depth: " + dominanceRankNaive.get(i).get(0).getDominanceDepth());
+    public static void printSortedByDominanceRank(List<Individual> individuals){
+        System.out.println("Dominance rank sort:");
+        Comparator<Individual> komparator = new DominanceRankComparator();
+        individuals.sort(komparator);
+        for( int i = 0 ; i < individuals.size() ; ++i ) {
+            System.out.println((i+1) + ".) dominance rank: " + individuals.get(i).getDominanceRank());
         }
+    }
 
+    public static void printSortedByDominanceDepth(List<Individual> individuals){
+        System.out.println("Dominance depth sort:");
+        Comparator<Individual> komparator = new DominanceDepthComparator();
+        individuals.sort(komparator);
+        for( int i = 0 ; i < individuals.size() ; ++i ) {
+            System.out.println((i+1) + ".) dominance depth: " + individuals.get(i).getDominanceDepth());
+        }
+    }
+
+    public static void printSortedByDominanceCount(List<Individual> individuals){
+        System.out.println("Dominance count sort:");
+        Comparator<Individual> komparator = new DominanceCountComparator();
+        individuals.sort(komparator);
+        for( int i = 0 ; i < individuals.size() ; ++i ) {
+            System.out.println((i+1) + ".) dominance count: " + individuals.get(i).getDominanceCount());
+        }
     }
 
     public static void simulate(List<Individual> individuals) throws IOException {
@@ -85,6 +106,38 @@ public class HeuristicOptimizationMain {
         writeToFile(kungIdentifiedSet, "kung.txt");
     }
 
+    public static List<Individual> generateDominanceDepthLayersByNaive(List<Individual> initialIndividualsNaive){
+        int counter = 0;
+        List<List<Individual>> dominanceRankNaive = new ArrayList<>();
+        while( initialIndividualsNaive.size() > 0 ) {
+            dominanceRankNaive.add(new ArrayList<Individual>(simulateByNaiveAlgorithm(initialIndividualsNaive)));
+            for( int i = 0 ; i < dominanceRankNaive.get(counter).size() ; ++i ){
+                if( initialIndividualsNaive.contains(dominanceRankNaive.get(counter).get(i))){
+                    dominanceRankNaive.get(counter).get(i).setDominanceDepth(counter+1);
+                    initialIndividualsNaive.remove(dominanceRankNaive.get(counter).get(i));
+                }
+            }
+            counter++;
+        }
+        return dominanceRankNaive.stream().flatMap(List::stream).collect(Collectors.toList());
+    }
+
+    public static List<Individual> generateDominanceDepthLayersByKung(List<Individual> initialIndividualsKung){
+        int counter = 0;
+        List<List<Individual>> dominanceRankKung = new ArrayList<>();
+        while( initialIndividualsKung.size() > 0 ) {
+            dominanceRankKung.add(new ArrayList<Individual>(simulateByKungAlgorithm(initialIndividualsKung)));
+            for( int i = 0 ; i < dominanceRankKung.get(counter).size() ; ++i ){
+                if( initialIndividualsKung.contains(dominanceRankKung.get(counter).get(i))){
+                    dominanceRankKung.get(counter).get(i).setDominanceDepth(counter+1);
+                    initialIndividualsKung.remove(dominanceRankKung.get(counter).get(i));
+                }
+            }
+            counter++;
+        }
+        return dominanceRankKung.stream().flatMap(List::stream).collect(Collectors.toList());
+    }
+
     public static List<Individual> simulateByNaiveAlgorithm(List<Individual> individuals){
         individuals = individuals.stream().distinct().collect(Collectors.toList());
         HeuristicComparator naiveComparator = new NaiveComparator();
@@ -93,6 +146,7 @@ public class HeuristicOptimizationMain {
     }
 
     public static List<Individual> simulateByKungAlgorithm(List<Individual> individuals){
+        individuals = individuals.stream().distinct().collect(Collectors.toList());
         Collections.sort(individuals);
         HeuristicComparator kungComparator = new KungComparator();
         List<Individual> kungIdentifiedSet = kungComparator.identifyDominatedSet(individuals);
